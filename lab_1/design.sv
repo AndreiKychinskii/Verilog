f94377e0bfdf62d29862b68954ea6d5089b3acfb
Andrei Kychinskii <a_kychinskii@yahoo.com>
2025-10-19 12:18:50 +0300

    lab_1: 1st and full realisation of HW_1

--- lab_1/design.sv
+++ lab_1/design.sv
@@ -1,23 +1,90 @@
-// Code your design here
-module element_or(input a, input b, output out);
-  assign out = a | b;
-endmodule
-
-module element_not(input a, output out);
-  // or
-  // assign not_out = !a;
-  assign out = ~a;
-endmodule
-
-module element_and(input a, input b, output out);
-  assign out = a & b;
-endmodule
-
-module element_d_flip_flop(input data, input clock, output logic out);
-  always @(posedge clock)
-    begin
-      // blocking assignment:
-      // d_ff_out = data;
-      out <= data;
+// ADC Convert Function Part
+function logic [7:0] adc_convert(input real voltage, output logic [7:0] result);
+    real coefficient = 255.0;
+    if ((voltage < 0.0) || (voltage > 1.0)) begin
+        result = 8'b0;
     end
-endmodule
+    else begin
+      $display("voltage * coefficient = %.2f", voltage * coefficient);
+      result = $rtoi($ceil(voltage * coefficient));
+    end
+    return result;
+endfunction
+
+// Generate Pulse Task Part
+task generate_pulse (input int delay_before_rising_edge,
+                   input int pulse_duration,
+                   input int repeat_counter,
+                   output logic signal);
+  int delay_b4_rising_edge;
+  int duration;
+  int num_of_pulses;
+
+  num_of_pulses = repeat_counter;
+
+  while (num_of_pulses > 0) begin
+      delay_b4_rising_edge = delay_before_rising_edge;
+    while (delay_b4_rising_edge > 1) begin
+          @(posedge home_work_1.clk);
+          signal = 1'b0;
+          delay_b4_rising_edge = delay_b4_rising_edge - 1;
+      end
+      duration = pulse_duration;
+    while (duration >= 0) begin
+          @(posedge home_work_1.clk);
+          signal = 1'b1;
+          duration = duration - 1;
+      end
+      signal = 1'b0;
+      num_of_pulses = num_of_pulses - 1;
+  end
+endtask
+
+// Execute Task Part
+typedef enum logic[2:0] {ADD, SUBTRACT, XOR, AND} operation_code;
+
+typedef struct {
+    logic [2:0] operation_code;
+    logic [6:0] val_a;
+    logic [6:0] val_b;
+} cmd;
+
+task execute_cmd (input cmd command,
+                input time delay_before_execute,
+                output logic [6:0] result);
+  #delay_before_execute;
+  unique case (command.operation_code)
+      ADD: result = do_add(command.val_a, command.val_b);
+      SUBTRACT: result = do_subtract(command.val_a, command.val_b);
+      XOR: result = do_xor(command.val_a, command.val_b);
+      AND: result = do_and(command.val_a, command.val_b);
+  endcase
+endtask
+
+function logic [6:0] do_add(input logic [6:0] a, input logic [6:0] b);
+    logic [6:0] res;
+    $display("hello from do_add(...)");
+    res = a + b;
+    return res;
+endfunction
+
+function logic [6:0] do_subtract(input logic [6:0] a, input logic [6:0] b);
+    logic [6:0] res;
+    $display("hello from do_subtract(...)");
+    res = a - b;
+    return res;
+endfunction
+
+function logic [6:0] do_xor(input logic [6:0] a, input logic [6:0] b);
+    logic [6:0] res;
+    $display("hello from do_xor(...)");
+    res = a ^ b;
+    return res;
+endfunction
+
+function logic [6:0] do_and(input logic [6:0] a, input logic [6:0] b);
+    logic [6:0] res;
+    $display("hello from do_and(...)");
+    res = a & b;
+    return res;
+endfunction
--- lab_1/testbench.sv
+++ lab_1/testbench.sv
@@ -1,56 +1,186 @@
-// Code your testbench here
-// or browse Examples
-`define HIGH_LEVEL 1
-`define LOW_LEVEL 0
-`define NUMBER_OF_STATES_TO_TEST 8
+module home_work_1;
+  logic clk = 1'b0;
+  always
+      begin
+          clk = 1; #5; clk = 0; #5;
+      end
 
-module enable_module;
-	logic enable_i;
-	logic clk_i;
-	logic d_ff_1_out;
-	logic d_ff_2_out;
-	logic ov_fault_i;
-	logic uv_fault_i;
-	logic oc_fault_i;
-	logic or_1_out;
-	logic or_2_out;
-	logic not_out;
-	logic enable_o;
-
-	element_d_flip_flop d_ff_1(.data(enable_i), .clock(clk_i), .out(d_ff_1_out));
-	element_d_flip_flop d_ff_2(.data(d_ff_1_out), .clock(clk_i), .out(d_ff_2_out));
-    element_or el_or_1(.a(ov_fault_i), .b(uv_fault_i), .out(or_1_out));
-	element_or el_or_2(.a(or_1_out), .b(oc_fault_i), .out(or_2_out));
-	element_not el_not(.a(or_2_out), .out(not_out));
-	element_and el_and(.a(d_ff_2_out), .b(not_out), .out(enable_o));
-
+	// Work with variables
 	initial begin
-	    enable_i = `HIGH_LEVEL;
-        #80;
-        enable_i = `LOW_LEVEL;
+		// 1
+		bit [3:0] just_bit_var = 4'b1111;
+		int just_int_var = just_bit_var;
+        $display("just_bit_var (decimal) = %0d", just_bit_var);
+        $display("just_int_var (decimal) = %0d", just_int_var);
+        // Поясніть, чому результат завжди інтерпретується як unsigned.
 	end
-
 	initial begin
-		for (logic [3:0] fault_states = 0; fault_states < `NUMBER_OF_STATES_TO_TEST; fault_states++) begin
-			ov_fault_i <= fault_states[0];
-			uv_fault_i <= fault_states[1];
-			oc_fault_i <= fault_states[2];
-			#20;
-		end
+		// 2
+		logic [3:0] just_logic_value = 4'b1111;
+		logic signed [3:0] just_signed_logic_value = just_logic_value;
+        $display("just_logic_value (decimal) = %0d", just_logic_value);
+        $display("just_signed_logic_value (decimal) = %0d", just_signed_logic_value);
 	end
-
 	initial begin
-		clk_i = 0;
-		forever begin
-		  #5 clk_i = !clk_i;
-		end
+		// 3
+		logic [3:0] logic_with_x_bit = 4'b10x1;
+		logic [3:0] logic_with_z_bit = 4'b01z0;
+        $display("logic_with_x_bit (decimal) = %0d", logic_with_x_bit);
+        $display("logic_with_z_bit (decimal) = %0d", logic_with_z_bit);
 	end
-
 	initial begin
-		$dumpfile("wave1.vcd");
-		$dumpvars(0);
-		#150;
-		$finish();
-	end
+		// 4
+		bit just_bit_var_1 = 1'b1;
+		bit just_bit_var_2 = 1'b1;
+		bit one_bit_width_sum = just_bit_var_1 + just_bit_var_2;
+		bit [1:0] two_bit_width_sum = just_bit_var_1 + just_bit_var_2;
+        $display("one_bit_width_sum (decimal) = %0d", one_bit_width_sum);
+        $display("two_bit_width_sum (decimal) = %0d", two_bit_width_sum);
+    end
 
+    // Packed array
+    // A multidimensional packed array is still a set of contiguous bits but are also segmented into smaller groups.
+    initial begin
+	    logic [3:0][7:0] registers;
+	    logic [1:0][7:0] msb_slice;
+	    logic [1:0][7:0] lsb_slice;
+	    registers = 32'hFACE_CAFE;
+	    for (int i = 0; i < $size(registers); i++) begin
+		    $display ("registers[%0d] = %b (0x%0h)", i, registers[i], registers[i]);
+	    end
+        msb_slice = registers[3:2];
+	    lsb_slice = registers[1:0];
+	    // Print msb_slice and lsb_slice
+        $display("msb_slice = 0x%0h", msb_slice);
+        $display("lsb_slice = 0x%0h", lsb_slice);
+        // Print MSB & LSB of msb_slice
+        $display("MSB of msb_slice = 0x%0h", msb_slice[1]);
+        $display("LSB of msb_slice = 0x%0h", msb_slice[0]);
+    end
+
+    // Unpacked array
+    initial begin
+	    byte image[4][4];
+	    // Create matrix
+	    foreach (image[row_idx]) begin
+		    foreach (image[row_idx][col_idx]) begin
+		    	image[row_idx][col_idx] = row_idx * col_idx;
+		    end
+	    end
+	    // Print matrix
+	    foreach (image[row_idx]) begin
+		    foreach (image[row_idx][col_idx]) begin
+			    $display ("image[%0d][%0d] = 0x%0h", row_idx, col_idx, image[row_idx][col_idx]);
+		    end
+	    end 
+    end
+
+    // Associative array
+    initial begin
+      string key;
+	    byte mac_addresses[string];
+	    mac_addresses["AA:BB:CC:DD:EE:01"] = 1;
+	    mac_addresses["AA:BB:CC:DD:EE:02"] = 2;
+	    mac_addresses["AA:BB:CC:DD:EE:03"] = 3;
+	    mac_addresses["AA:BB:CC:DD:EE:04"] = 9;
+	    mac_addresses["AA:BB:CC:DD:EE:05"] = 7;
+	    mac_addresses["AA:BB:CC:DD:EE:06"] = 8;
+	    key = "AA:BB:CC:DD:EE:02";
+	    if (mac_addresses.exists(key)) begin
+	    	$display ("mac_addresses[%s] = 0x%0h", key, mac_addresses[key]);
+	    end
+	    else begin
+		    $display ("mac_addresses[%s] doesn't exist!", key);
+	    end
+			key = "AA:BB:CC:DD:EE:11";
+	    if (mac_addresses.exists(key)) begin
+	    	$display ("mac_addresses[%s] = 0x%0h", key, mac_addresses[key]);
+	    end
+	    else begin
+		    $display ("mac_addresses[%s] doesn't exist!", key);
+	    end
+	    mac_addresses["AA:BB:CC:DD:EE:10"] = 5;
+	    mac_addresses.delete("AA:BB:CC:DD:EE:01");
+	    mac_addresses["AA:BB:CC:DD:EE:05"] = 10;
+      $display ("Iterate over all members of associative array");
+	    if (mac_addresses.first(key)) begin
+		    $display ("mac_addresses[%s] = 0x%0h", key, mac_addresses[key]);
+          for (byte address = 0; address < mac_addresses.size() - 1; address++) begin
+            mac_addresses.next(key);
+						$display ("mac_addresses[%s] = 0x%0h", key, mac_addresses[key]);
+					end
+	    end
+    end
+
+    // Queue
+    initial begin
+	    int unbounded_queue[$] = {101, 102, 103, 104};
+	    unbounded_queue.push_back(105);
+	    unbounded_queue.push_back(106);
+	    unbounded_queue.push_front(200);
+	    $display("1st element from front of queue is %0d", unbounded_queue.pop_front());
+	    $display("2nd element from front of queue is %0d", unbounded_queue.pop_front());
+	    $display("last element from end of queue is %0d", unbounded_queue.pop_back());
+	    for (int idx = 0, limit = unbounded_queue.size(); idx < limit; idx++ ) begin
+		    $display("unbounded_queue[%0d] is %0d", idx, unbounded_queue[idx]);
+	    end
+	    $display("unbounded_queue.size() before unbounded_queue.delete() is %0d", unbounded_queue.size());
+	    unbounded_queue.delete();
+	    $display("unbounded_queue.size() after unbounded_queue.delete() is %0d", unbounded_queue.size());
+    end
+
+    // Function
+    initial begin
+	    logic [7:0] result;
+	    real voltages[7] = {-0.1, 0.0, 0.25, 0.5, 0.75, 1.0, 1.1};
+	    for (int idx = 0; idx < 7; idx++) begin
+		    $display("adc_convert(%.2f) returns %0d", voltages[idx], adc_convert(voltages[idx], result));
+	    end
+    end
+
+    // Generate Pulse Task
+    initial begin
+	        logic signal = 1'b0;
+          typedef struct {
+              int delay_before_rising_edge;
+              int pulse_duration;
+              int repeat_cnt;
+          } generate_pulse_args_t;
+
+      generate_pulse_args_t generate_pulse_args[3] = '{
+		        '{delay_before_rising_edge: 2, pulse_duration: 5, repeat_cnt: 1},
+		        '{delay_before_rising_edge: 2, pulse_duration: 2, repeat_cnt: 3},
+		        '{delay_before_rising_edge: 6, pulse_duration: 6, repeat_cnt: 2}
+          };
+
+          foreach(generate_pulse_args[i])
+              begin
+                generate_pulse(generate_pulse_args[i].delay_before_rising_edge,
+                               generate_pulse_args[i].pulse_duration,
+                               generate_pulse_args[i].repeat_cnt,
+                               signal);
+              end
+    end
+
+    initial begin
+        $dumpfile("wave1.vcd");
+        $dumpvars(0);
+        #550;
+        $finish();
+    end
+
+  	// Execute Task
+    initial begin
+	    logic [6:0] result;
+	    cmd cmd_queue[$] = '{
+	    	'{ADD, 10, 3},
+	    	'{SUBTRACT, 20, 5},
+	    	'{XOR, 7, 3},
+	    	'{AND, 15, 7}
+	    };
+	    while (cmd_queue.size()) begin
+        execute_cmd(cmd_queue.pop_front(), 10, result);
+			  $display("cmd_queue: time of execution is [%0d], result is %0d", 10, result);
+	    end
+    end
 endmodule

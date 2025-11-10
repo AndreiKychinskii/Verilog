32eb3148ab3617f9eaca07bfe1e570f2a8e417ef
Andrei Kychinskii <a_kychinskii@yahoo.com>
2025-11-10 22:07:02 +0200

    hw2: testbench was proved

--- lab_2/design.sv
+++ lab_2/design.sv
@@ -1,32 +1,28 @@
 interface clk_if();
-    logic clk = 1'b0;
+  logic clk = 1'b0;
 	logic rstb = 1'b1;
 	logic clk_en;
 	time period = 10;
-    logic is_started = 1'b0;
+  logic is_started = 1'b0;
 
 	task start_clock();
-      $display("Hello from start_clock(...)");
-      if (!is_started) begin
-        clk_en = 1'b1;
-        is_started = 1'b1;
-        fork
-          forever begin
-		          #(period / 2);
-		          if (clk_en) begin
-		              clk = ~clk;
-		          end else begin
-		              clk = 1'b0;
-		          end
-          end
-        join_none
-      end else begin
-        clk_en = 1'b1;
-      end
+    $display("Hello from start_clock(...)");
+    if (!is_started) begin
+      clk_en = 1'b1;
+      is_started = 1'b1;
+      fork
+        forever begin
+	          #(period / 2);
+	          clk = clk_en ? ~clk : 1'b0;
+        end
+      join_none
+    end else begin
+      clk_en = 1'b1;
+    end
 	endtask
 
 	task stop_clock();
-        $display("Hello from stop_clock(...)");
+    $display("Hello from stop_clock(...)");
 		clk_en = 1'b0;
 	endtask
 
@@ -67,3 +63,46 @@ interface clk_if();
 	endtask
 
 endinterface
+
+interface counter_if();
+	logic clk;
+	logic rstb;
+	logic cnt_en;
+  logic [3:0] cnt;
+
+	clocking cb0 @(posedge clk);
+		input #0 cnt;
+		output #0 cnt_en;
+	endclocking
+
+	clocking cb1 @(posedge clk);
+		input #1step cnt;
+		output #20ns cnt_en;
+	endclocking
+
+	clocking cb2 @(posedge clk);
+		input #100ns cnt;
+		output #50 cnt_en;
+	endclocking
+
+	clocking cb3 @(posedge clk);
+		input #200ns cnt;
+		output #100 cnt_en;
+	endclocking
+
+endinterface
+
+module counter(input logic clk,
+						   input logic rstb,
+						   input logic cnt_en,
+               output logic [3:0] cnt);
+always @(posedge clk or negedge rstb) begin
+    if (~rstb) begin
+        cnt <= 4'b0000;
+    end else if (cnt_en) begin
+      cnt <= cnt + 1;
+      $display ("pulse cnt = 0x%0h", cnt);
+    end
+	end
+
+endmodule
--- lab_2/testbench.sv
+++ lab_2/testbench.sv
@@ -1,27 +1,77 @@
 module home_work_2;
-  // Instantiate the interface
+  // Instantiate the interfaces
   clk_if test_clk_if();
-
   initial begin
-      test_clk_if.set_frequency_in_Mhz(100);
-      test_clk_if.start_clock();
-//       test_clk_if.drive_reset(10, 5);
-      #100;
-      test_clk_if.drive_reset_async(20, 50);
-      #100;
-      test_clk_if.stop_clock();
-      #50;
-      test_clk_if.set_frequency_in_Mhz(10);
-      test_clk_if.start_clock();
-      #50;
-      test_clk_if.stop_clock();
+    test_clk_if.set_frequency_in_Mhz(100);
+    test_clk_if.start_clock();
+    test_clk_if.drive_reset_async(10, 20);
+    #50;
+    test_clk_if.drive_reset(10, 5);
+    #50;
+    test_clk_if.stop_clock();
+    #20;
   end
 
+  counter_if test_counter_if();
+
+  assign test_counter_if.clk = test_clk_if.clk;
+  assign test_counter_if.rstb = test_clk_if.rstb;
+  
+  counter test_counter (
+    .clk(test_counter_if.clk),
+    .rstb(test_counter_if.rstb),
+    .cnt_en(test_counter_if.cnt_en),
+    .cnt(test_counter_if.cnt)
+  );
+
   initial begin
-      $dumpfile("wave1.vcd");
-      $dumpvars(0);
-      #550;
-      $finish();
+    #120;
+    // Frequency setup
+    test_clk_if.set_frequency_in_Mhz(100);
+    // Run clock generation
+    test_clk_if.start_clock();
+    // Clock sync reset
+    test_clk_if.drive_reset(5, 5);
+    // Enable counter interface
+    test_counter_if.cnt_en <= 1;
+    // A few clocks pause
+    repeat(5) @(test_counter_if.cb0);
+    // Analysis via fork-join
+    fork
+      begin
+        @(test_counter_if.cb0);
+        $display ("cb0.cnt = 0x%0h", test_counter_if.cb0.cnt);
+      end
+      begin
+        @(test_counter_if.cb1);
+        $display ("cb1.cnt = 0x%0h", test_counter_if.cb1.cnt);
+      end
+      begin
+        @(test_counter_if.cb2);
+        $display ("cb2.cnt = 0x%0h", test_counter_if.cb2.cnt);
+      end
+      begin
+        @(test_counter_if.cb3);
+        $display ("cb3.cnt = 0x%0h", test_counter_if.cb3.cnt);
+      end
+    join
+    // Set cnt_en via different clocking block
+    @(test_counter_if.cb0); test_counter_if.cb0.cnt_en <= 1;
+    @(test_counter_if.cb0); test_counter_if.cb0.cnt_en <= 0;
+    @(test_counter_if.cb1); test_counter_if.cb1.cnt_en <= 1;
+    @(test_counter_if.cb1); test_counter_if.cb1.cnt_en <= 0;
+    @(test_counter_if.cb2); test_counter_if.cb2.cnt_en <= 1;
+    @(test_counter_if.cb2); test_counter_if.cb2.cnt_en <= 0;
+    @(test_counter_if.cb3); test_counter_if.cb3.cnt_en <= 1;
+    @(test_counter_if.cb3); test_counter_if.cb3.cnt_en <= 0;
+    #200ns $finish;
   end
 
+  // Waveform dump and finish
+  initial begin
+    $dumpfile("wave1.vcd");
+    $dumpvars(0);  // Dump everything in this module
+    #1000;  // adjust as needed
+    $finish();
+  end
 endmodule

4f5122d5080631c0c176c89dcd86d96d30d17f30
Andrei Kychinskii <a_kychinskii@yahoo.com>
2026-01-14 06:39:09 +0200

    hw4: cmd and data_packet: basic realizations

--- lab_4/src/design.sv
+++ lab_4/src/design.sv
@@ -1,4 +1,149 @@
-// 
+// =======================================================================================================================
+// =======================================================================================================================
+
+class data_packet;
+	rand int id;
+	rand int data[];
+
+	constraint data_size {this.data.size() inside {[4:8]};}
+	constraint data_sum {this.data.sum() inside {[20:40]};}
+	constraint data_elem_1 {foreach (data[i])
+		data[i] inside {[1:10]};
+	}
+	constraint data_elem_2 {foreach (this.data[i])
+		this.data[i] > this.data[i - 1];
+	}
+
+	extern function void display();
+
+endclass : data_packet
+
+function void data_packet::display();
+	$display("==================================");
+	$display("packet id = %0d", this.id);
+	$display("==================================");
+	$display("data.sum() = %0d", this.data.sum());
+	foreach (this.data[i]) begin
+		$display("data[%0d] = %0d", i, this.data[i]);
+	end
+endfunction
+
+class data_packet_queue;
+	// local parameter int MAX_PACKETS = 6;
+
+	rand data_packet data_packets[];
+
+	constraint data_packets_size {this.data_packets.size() inside {[3:6]};}
+
+	constraint data_packet_id_is_limited {foreach (data_packets[i])
+		data_packets[i].id inside {[1:20]};
+	}
+
+	constraint data_packet_id_is_unique {
+		foreach (data_packets[i])
+			foreach (data_packets[j])
+				if (i != j)
+					data_packets[i].id != data_packets[j].id;
+	}
+
+	function new();
+		// data_packets = new[MAX_PACKETS];
+		data_packets = new[6];
+		foreach (data_packets[i]) begin
+			data_packets[i] = data_packet::new();
+		end
+	endfunction
+
+endclass : data_packet_queue
+
+// =======================================================================================================================
+// =======================================================================================================================
+
+typedef enum logic [2:0] {
+	ADD = 0,
+	SUB = 1,  // ??? constrants
+	AND = 2,
+	XOR = 3,
+	MUL = 4,
+	DIV = 5
+} opcode_t;
+
+class cmd;
+	randc opcode_t opcode_e;
+	rand logic [15:0] op_a;
+	rand logic [15:0] op_b;
+	rand int unsigned latency_in_ns;
+	logic [15:0] result;
+
+	constraint c_opcode_e {
+		opcode_e inside {[0:5]};
+	};
+
+	constraint c1_latency_in_ns {
+		(opcode_e == ADD) -> latency_in_ns inside {[1:10]};
+		(opcode_e == SUB) -> latency_in_ns inside {[1:10]};
+		(opcode_e == AND) -> latency_in_ns inside {[1:10]};
+		(opcode_e == XOR) -> latency_in_ns inside {[1:10]};
+	};
+
+	constraint c2_latency_in_ns {
+		(opcode_e == MUL) -> latency_in_ns inside {[20:100]};
+		(opcode_e == DIV) -> latency_in_ns inside {[20:100]};
+	};
+
+	constraint c_op_b {
+		op_b == 0 -> op_a == 0;
+	};
+
+	constraint c_bit_width_add {
+	    (opcode_e == ADD) -> op_a inside {[16'h0000 : 16'h7FFF]};
+	    (opcode_e == ADD) -> op_b inside {[16'h0000 : 16'h7FFF]};
+	}
+
+	constraint c_bit_width_mul {
+		(opcode_e == MUL) -> op_a inside {[0:255]};
+		(opcode_e == MUL) -> op_b inside {[0:255]};
+	}
+
+endclass : cmd
+
+task automatic execute_randomized_cmd(ref cmd ptr_cmd);
+	begin
+		realtime start_time, end_time;
+		string op_code_like_str;
+		if (!ptr_cmd.randomize()) begin
+			$display("ERROR: FAIL of randomization is detected!");
+			$finish();
+			// disable;
+		end
+		start_time = $realtime;
+		#(ptr_cmd.latency_in_ns);
+		case (ptr_cmd.opcode_e)
+			ADD: ptr_cmd.result = ptr_cmd.op_a + ptr_cmd.op_b;
+			SUB: ptr_cmd.result = ptr_cmd.op_a - ptr_cmd.op_b;
+			AND: ptr_cmd.result = ptr_cmd.op_a & ptr_cmd.op_b;
+			XOR: ptr_cmd.result = ptr_cmd.op_a ^ ptr_cmd.op_b;
+			MUL: ptr_cmd.result = ptr_cmd.op_a * ptr_cmd.op_b;
+			DIV: ptr_cmd.result = ptr_cmd.op_a / ptr_cmd.op_b;
+			default: ptr_cmd.result = 0;
+		endcase
+		end_time = $realtime;
+		case (ptr_cmd.opcode_e)
+			ADD: op_code_like_str = "ADD";
+			SUB: op_code_like_str = "SUB";
+			AND: op_code_like_str = "AND";
+			XOR: op_code_like_str = "XOR";
+			MUL: op_code_like_str = "MUL";
+			DIV: op_code_like_str = "DIV";
+			default: ptr_cmd.result = 0;
+		endcase
+		$display("%0d = %0d %s %0d, delta_time = %0t ns", ptr_cmd.result, ptr_cmd.op_a, op_code_like_str, ptr_cmd.op_b, end_time - start_time);
+	end
+endtask
+
+// =======================================================================================================================
+// =======================================================================================================================
+
 interface histogram_if;
 	logic clk;
 	real generated_cnt;
@@ -86,14 +231,6 @@ task automatic visualize_hist(ref distribution_model ptr_dm, int unsigned stats_
 		key_idx = key_idx + 1;
 	end
 endtask
-
-// Task visualize_hist(int unsigned stats_rand[int])
-// Функціональність:
-// 1. Робить початкову затримку у 100 клок тактів
-// 2. Для кожного значення від 1 до 100 на кожному posedge тактового сигналу
-// 	передавати статистичні дані (ключ асоціативного масиву ->
-// 	histogram_if.generated_value, значення асоціативного масиву ->
-// 	histogram_if.generated_cnt);
 
 // =======================================================================================================================
 // =======================================================================================================================
--- lab_4/src/top.sv
+++ lab_4/src/top.sv
@@ -1,6 +1,9 @@
 module home_work_4;
   histogram_if histogram_if_tb();
-  distribution_model dm;
+  // distribution_model dm;
+  cmd tst_cmd;
+  data_packet tst_data_packet;
+  data_packet_queue tst_data_packet_queue;
 
   // Clocking
   logic [7:0] clk_period = 20;
@@ -9,29 +12,64 @@ module home_work_4;
         histogram_if_tb.clk = 1; #(clk_period / 2); histogram_if_tb.clk = 0; #(clk_period / 2);
       end
 
-  initial begin
-	dm = new(histogram_if_tb);
-	repeat(10_000) begin
-		randomize_rand_values(dm);
-	end
-	visualize_hist(dm, dm.stats_rand);
-	visualize_hist(dm, dm.stats_randc);
-	visualize_hist(dm, dm.stats_dist);
-
-	repeat(100_000) begin
-		randomize_rand_values(dm);
-	end
-	visualize_hist(dm, dm.stats_rand);
-	visualize_hist(dm, dm.stats_randc);
-	visualize_hist(dm, dm.stats_dist);
-  end
-
-  // // Waveform dump and finish
   // initial begin
-  //   $dumpfile("wave1.vcd");
-  //   $dumpvars(0);  // Dump everything in this module
-  //   #1000;  // adjust as needed
-  //   $finish();
+	// dm = new(histogram_if_tb);
+	// repeat (10_000) begin
+	// 	randomize_rand_values(dm);
+	// end
+	// visualize_hist(dm, dm.stats_rand);
+	// visualize_hist(dm, dm.stats_randc);
+	// visualize_hist(dm, dm.stats_dist);
+
+	// repeat (100_000) begin
+	// 	randomize_rand_values(dm);
+	// end
+	// visualize_hist(dm, dm.stats_rand);
+	// visualize_hist(dm, dm.stats_randc);
+	// visualize_hist(dm, dm.stats_dist);
   // end
+
+  // initial begin
+  //   tst_cmd = new();
+  //   repeat (20) begin
+  //     execute_randomized_cmd(tst_cmd);
+  //   end
+  // end
+
+  initial begin
+    // no id constraints in data_packet
+    tst_data_packet = new();
+    repeat (3) begin
+      if (tst_data_packet.randomize()) begin
+        tst_data_packet.display();
+      end else begin
+        $display("ERROR: FAIL of randomization of tst_data_packet is detected!");
+      end
+    end
+    $display("\n=== tst_data_packet_queue example ===");
+    repeat (3) begin
+      tst_data_packet_queue = new();
+      if (tst_data_packet_queue.randomize()) begin
+        foreach (tst_data_packet_queue.data_packets[i]) begin
+          tst_data_packet_queue.data_packets[i].display();
+        end
+      end else begin
+        $display("ERROR: FAIL of randomization of tst_data_packet_queue is detected!");
+      end
+    end
+    $display("\n=== inline constraints example ===");
+    tst_data_packet_queue = new();
+    tst_data_packet_queue.randomize() with {data_packets.size() == 4;};
+    foreach (tst_data_packet_queue.data_packets[i]) begin
+      tst_data_packet_queue.data_packets[i].display();
+    end
+  end
+  // Waveform dump and finish
+  initial begin
+    $dumpfile("wave1.vcd");
+    $dumpvars(0);  // Dump everything in this module
+    #1000;  // adjust as needed
+    $finish();
+  end
 
 endmodule
